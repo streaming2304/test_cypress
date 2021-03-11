17aad78e3bdcdb03dc8535deb1a1583e32f92546
artit <615021000836@gmail.rmutk.ac.th>
2021-03-12 00:45:38 +0700

    Lap

---
+++ cypress/integration/examples/test.js
@@ -0,0 +1,10 @@
+describe('Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
+      cy.contains('type').click()
+      cy.get('.action-email')
+        .type('fake@email.com')
+    })
+  })
+  
+  
